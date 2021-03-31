e175f873f2011f66bdfa9925bec76ace63a5d420
sean <sean00mcc@gmail.com>
2021-03-31 14:33:18 -0400

    now we go back with the processLine and account for delim

--- main.go
+++ main.go
@@ -3,24 +3,28 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"time"
 )
 
-func playbackLogFile(playbackSpeed int, playbackFile string) bool {
+func playbackLogFile(playbackSpeed int, playbackFile string, delim string, outFile string) bool {
 	file, err := os.Open(playbackFile)
 	if err != nil {
 		return false
 	}
 	defer file.Close()
+	p := Processor{
+		writer:  os.Stdout,
+		delim:   delim,
+		outFile: outFile,
+	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		time.Sleep(time.Duration(playbackSpeed) * time.Second)
-		fmt.Println(scanner.Text())
+		p.processLine(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -32,7 +36,7 @@ func playbackLogFile(playbackSpeed int, playbackFile string) bool {
 func main() {
 	addr := flag.String("addr", "localhost", "bind ip")
 	port := flag.Int("port", 5555, "bind port")
-	delim := flag.String("delim", ":", "What delimeter do you want to use? Pass empty string for no filtering")
+	delim := flag.String("delim", "", "What delimeter do you want to use? Pass empty string for no filtering")
 	outFile := flag.String("outFile", "logfile.log", "logfile name")
 	playback := flag.Bool("playback", false, "do you want to playback a log file")
 	playbackSpeed := flag.Int("playbackSpeed", 1, "how fast to play back, in seconds. 0 will dump it all at once")
@@ -40,7 +44,7 @@ func main() {
 	flag.Parse()
 
 	if *playback {
-		playbackLogFile(*playbackSpeed, *playbackFile)
+		playbackLogFile(*playbackSpeed, *playbackFile, *delim, *outFile)
 	} else {
 		servStr := *addr + ":" + strconv.Itoa(*port)
 		tcpServ, err := NewServer(servStr, *delim, *outFile)
